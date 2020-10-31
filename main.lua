ed51f0092f6befa626107de3fa63b1b1ba226c13
christophergale <cdgale@googlemail.com>
2020-10-31 15:33:21 +0000

    Audio first implementation

--- main.lua
+++ main.lua
@@ -13,6 +13,13 @@ function love.load()
             love.graphics.newImage("assets/car/06.png"),
             love.graphics.newImage("assets/car/07.png"),
         },
+        sounds =
+        {
+            idle_engine = love.audio.newSource('assets/sounds/large/Idle_Engine.wav', 'static'),
+            rev_engine_loop = love.audio.newSource('assets/sounds/large/Rev_Loop.wav', 'static'),
+            skid_short_01 = love.audio.newSource('assets/sounds/large/Skid_Short.wav', 'static'),
+            skid_long = love.audio.newSource('assets/sounds/large/Skid_Long_01.wav', 'static'),
+        },
         position = Vector2:New{ x = 400, y = 300 },
         velocity = Vector2:New{x = 0, y = 0},
         rotation = 0,
@@ -29,9 +36,20 @@ function love.load()
     maxSkidMarks = 120
 
     dustParticles = {}
-    maxDustParticles = 60
+    maxDustParticles = 45
 
     love.mouse.setVisible(false)
+
+    setSoundParameters(car.sounds.idle_engine, 1.25, nil, true)
+    setSoundParameters(car.sounds.rev_engine_loop, nil, 0.5, true)
+    setSoundParameters(car.sounds.skid_long, 1.25, 0.75, true)
+
+end
+
+function setSoundParameters(sound, pitch, volume, looping)
+    sound:setPitch(pitch or 1)
+    sound:setVolume(volume or 1)
+    sound:setLooping(looping or false)
 end
 
 function love.update(dt)
@@ -53,12 +71,29 @@ function love.update(dt)
         if car.velocity:Magnitude() < 6 then
             addSkidMarks()
             addDust()
+            car.sounds.skid_short_01:setPitch(love.math.random(1, 1.5))
+            car.sounds.skid_short_01:setVolume(0.75, 1.0)
+            love.audio.play(car.sounds.skid_short_01)
+        else
+            love.audio.stop(car.sounds.skid_short_01)
         end
+
+        car.sounds.rev_engine_loop:setPitch(math.max(0.75, 0.2 * car.velocity:Magnitude()))
+        car.sounds.rev_engine_loop:setVolume(0.075 * car.velocity:Magnitude())
+        love.audio.play(car.sounds.rev_engine_loop)
+        love.audio.stop(car.sounds.idle_engine)
+    else
+        love.audio.play(car.sounds.idle_engine)
+        love.audio.stop(car.sounds.skid_short_01)
+        love.audio.stop(car.sounds.rev_engine_loop)
     end
 
     if math.abs(math.deg(Vector2.FindAngleRadians(car.velocity, carToMouse))) > 20 and car.velocity:Magnitude() > 4 then
         addSkidMarks()
         addDust()
+        love.audio.play(car.sounds.skid_long)
+    else
+        love.audio.stop(car.sounds.skid_long)
     end
 
     if #skidMarks > maxSkidMarks then
@@ -81,6 +116,10 @@ function love.update(dt)
         if dust.opacity <= 0 then
             table.remove(dustParticles, 1)
         end
+    end
+
+    if target then
+        carToTarget = Vector2:New{x = target.position.x - car.position.x, y = target.position.y - car.position.y}:Normalize()
     end
 end
 
@@ -115,6 +154,14 @@ function love.draw()
     love.graphics.setColor(.4, .4, .4)
     love.graphics.circle('fill', love.mouse.getX(), love.mouse.getY(), 15, 15)
 
+    if target then
+        love.graphics.setColor(.5, .1, .1)
+        love.graphics.circle('fill', target.position.x, target.position.y, 30, 30)
+
+        love.graphics.setColor(1,1,1,1)
+        love.graphics.print(math.deg(Vector2.FindAngleRadians(target.initialVectorToCar, carToTarget)))
+    end
+
     for i, mark in ipairs(skidMarks) do
         love.graphics.setColor(.2, .2, .2)
         love.graphics.rectangle('fill', mark.x, mark.y, 6, 6)
@@ -130,4 +177,19 @@ function love.draw()
         love.graphics.setColor(1,1,1,1)
         love.graphics.draw(sprite, car.position.x, car.position.y - (i * 3), car.rotation, 3, 3, 8, 8)
     end
+end
+
+function createTarget()
+    local width, height = love.graphics.getDimensions()
+
+    target =
+    {
+        position = Vector2:New{x = love.math.random(120, width - 120), y = love.math.random(120, height - 120)}, 
+    }
+
+    target.initialVectorToCar = Vector2:New{x = target.position.x - car.position.x, y = target.position.y - car.position.y}
+end
+
+function love.keypressed(key)
+    createTarget()
 end
